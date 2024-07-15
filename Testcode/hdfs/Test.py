2e80352add4b85ff1e1ce31519f030503ee7e77c
trolololoojb <j-u-b+github@outlook.de>
2024-07-15 21:27:54 +0200

    test

--- Testcode/hdfs/Test.py
+++ Testcode/hdfs/Test.py
@@ -1,30 +1,9 @@
-from tokenizers import Tokenizer, models, trainers, pre_tokenizers, decoders, processors
+import re
 
-# Erstellen eines BPE Tokenizers
-tokenizer = Tokenizer(models.BPE())
+# Ursprünglicher String
+text = "Receiving block blk_-1608999687919862906 src: /10.250.19.102:54106 dest: /10.250.19.102:50010"
 
-# Definieren der PreTokenizer
-tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
+# Ersetzen aller Zahlen durch '0'
+modified_text = re.sub(r'\d', '0', text)
 
-# Definieren des Trainers
-trainer = trainers.BpeTrainer(vocab_size=100000, special_tokens=["<pad>", "<cls>", "<sep>", "<unk>"])
-
-# Pfad zur Logdatei
-log_file_path = r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hdfs_v1\content_list_hdfs.txt"
-
-# Training des Tokenizers
-tokenizer.train(files=[log_file_path], trainer=trainer)
-
-
-# Beispielhafte Logdaten
-log_data = [
-    "2023-07-15 12:34:56 ERROR Server failed to respond",
-    "2023-07-15 12:35:01 INFO User login successful"
-]
-
-# Tokenisierung der Logdaten
-for log in log_data:
-    encoded = tokenizer.encode(log)
-    print(f"Original Log: {log}")
-    print(f"Tokens: {encoded.tokens}")
-    print(f"Token IDs: {encoded.ids}")
+print(modified_text)
