a626666645a4f4cf975df284b29cce7bd95ea9f6
trolololoojb <j-u-b+github@outlook.de>
2024-07-18 19:12:37 +0200

    Verbesserung Tokenizer

--- Prodcode/01_General/DeepLearning/prepareTrainingData.py
+++ Prodcode/01_General/DeepLearning/prepareTrainingData.py
@@ -1,5 +1,7 @@
 import csv
+import os
 from tokenizers import Tokenizer, models, trainers, pre_tokenizers, decoders, processors
+import tqdm
 
 def generateTokenizer():
     # Erstellen eines BPE Tokenizers
@@ -38,9 +40,23 @@ def generateTokenizer():
     tokenizer.save(r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\Tokenizer\tokenizer.json")
 
 
+def delete_file(file_path):
+    """
+    Löscht die zu produzierenden Dateien um eine nicht gewollte Datenmanipulation zu vermeiden
+    """
+    try:
+        os.remove(file_path)
+        print(f"{file_path} wurde gelöscht.")
+    except FileNotFoundError:
+        print(f"{file_path} wurde nicht gefunden und konnte nicht gelöscht werden.")
+    except Exception as e:
+        print(f"Ein Fehler ist beim Löschen von {file_path} aufgetreten: {e}")
+
+
 def generateTokenizedData(tokenizer, input_filename, output_filename, chunk_size=1024):
     """
-    Verarbeitet eine Textdatei zeilenweise mit einem gegebenen Tokenizer und speichert das Ergebnis in einer CSV-Datei.
+    Verarbeitet eine Textdatei zeilenweise mit einem gegebenen Tokenizer und speichert die
+    Token-IDs zusammen mit den Tokens in einer CSV-Datei. Enthält eine Fortschrittsanzeige.
 
     Args:
     - tokenizer: Ein Tokenizer-Objekt, das eine Methode 'encode' hat.
@@ -48,37 +64,67 @@ def generateTokenizedData(tokenizer, input_filename, output_filename, chunk_size
     - output_filename: Der Pfad zur Ausgabedatei (.csv).
     - chunk_size: Die Größe des Chunks in Bytes, der aus der Datei gelesen wird.
     """
+    delete_file(output_filename)
+    # Dateigröße bestimmen für die Fortschrittsanzeige
+    total_size = os.path.getsize(input_filename)
+    processed_size = 0
 
     with open(input_filename, 'r', encoding='utf-8') as file, \
-         open(output_filename, 'w', newline='', encoding='utf-8') as out_file:
+         open(output_filename, 'a', newline='', encoding='utf-8') as out_file:
         writer = csv.writer(out_file)
         leftover = ''
         
-        while True:
-            # Lese einen Chunk aus der Datei
-            chunk = file.read(chunk_size)
-            if not chunk:
-                break
-            
-            # Füge den übrig gebliebenen Teil der letzten Iteration hinzu
-            chunk = leftover + chunk
-            
-            # Teile den Chunk in Zeilen
-            lines = chunk.split('\n')
-            
-            # Der letzte Teil ist möglicherweise eine unvollständige Zeile
-            leftover = lines.pop() if chunk[-1] != '\n' else ''
-            
-            # Verarbeite jede vollständige Zeile
-            for line in lines:
-                if line.strip():  # Ignoriere leere Zeilen
-                    encoded_line = tokenizer.encode(line)
-                    writer.writerow(encoded_line.tokens)  # Schreibe Tokens in die CSV-Datei
+        # Fortschrittsanzeige initialisieren
+        with tqdm.tqdm(total=total_size, unit='B', unit_scale=True, desc="Processing File") as pbar:
+            while True:
+                chunk = file.read(chunk_size)
+                if not chunk:
+                    break
+                chunk = leftover + chunk  # Hinzufügen des leftovers zum aktuellen Chunk
+                lines = chunk.split('\n')
+                leftover = lines.pop()  # Entfernen und Speichern der unvollständigen letzten Zeile
 
-        # Verarbeite die letzte Zeile, wenn sie vorhanden ist
+                for line in lines:
+                    text = line.split()
+                    # Tokenisierung des Textes
+                    sentence = []
+                    for word in text:
+                        word_output = tokenizer.encode(word)
+                        sentence.append(word_output.ids)
+                    writer.writerow(sentence)
+                
+                processed_size += len(chunk.encode('utf-8'))  # Update processed size
+                pbar.update(len(chunk.encode('utf-8')))
+
+        # Verarbeitung des letzten leftovers, falls vorhanden
         if leftover:
-            encoded_line = tokenizer.encode(leftover)
-            writer.writerow(encoded_line.tokens)
+            text = leftover.split()
+            sentence = []
+            for word in text:
+                word_output = tokenizer.encode(word)
+                sentence.append(word_output.ids)
+            writer.writerow(sentence)
+
+tokenizer = Tokenizer.from_file(r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\Tokenizer\tokenizer.json")
+content_file_path_list = [
+    r'C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\bgl_v1\content_list_bgl.txt',
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hdfs_v1\content_list_hdfs.txt",
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hpc_v1\content_list_hpc.txt",
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\proxifier_v1\content_list_proxifier.txt",
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\zookeeper_v1\content_list_zookeeper.txt"
+]
+
+tokenized_file_path_list = [
+    r'C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\bgl_v1\tokenized_list_bgl.csv',
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hdfs_v1\tokenized_list_hdfs.csv",
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hpc_v1\tokenized_list_hpc.csv",
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\proxifier_v1\tokenized_list_proxifier.csv",
+    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\zookeeper_v1\tokenized_list_zookeeper.csv"
+]
+
+for log_file, tokenized_file in zip(content_file_path_list, tokenized_file_path_list):
+    generateTokenizedData(tokenizer, log_file, tokenized_file, 10000)
 
 
-generateTokenizer()
+
+#generateTokenizedData(tokenizer, r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Testcode\hdfs\content_list_big.txt", r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Testcode\hdfs\token_list_test.csv", 10000)
