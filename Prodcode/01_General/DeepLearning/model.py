3378360dfc42bff74a6d22c93310cbe98e241e7b
trolololoojb <j-u-b+github@outlook.de>
2024-07-20 22:27:24 +0200

    model training without bpe

--- Prodcode/01_General/DeepLearning/model.py
+++ Prodcode/01_General/DeepLearning/model.py
@@ -1,9 +1,11 @@
 import csv
 import pandas as pd
 import tensorflow as tf
+from tokenizers import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, Bidirectional, GRU, Dense
-import tqdm
+from tensorflow.keras.layers import Embedding, Bidirectional, GRU, Dense, TimeDistributed, Dropout
+from sklearn.model_selection import train_test_split
 
 
 def generator_data(data_path: str, batch_size: int, epochs: int):
@@ -13,26 +15,25 @@ def generator_data(data_path: str, batch_size: int, epochs: int):
             yield df[['x']].values, df['y'].values
 
 
-import numpy as np
 
 # Beispiel X und Y Daten
 X_data_files = [
-    r'C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\bgl_v1\tokenized_list_bgl.csv',
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hdfs_v1\tokenized_list_hdfs.csv",
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hpc_v1\tokenized_list_hpc.csv",
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\proxifier_v1\tokenized_list_proxifier.csv",
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\zookeeper_v1\tokenized_list_zookeeper.csv"
+    r'/home/johann/github/LogAnalyzer/Datensätze/Vorbereitete Daten - Beispiel/bgl_v1/tokenized_list_bgl.csv',
+    r"/home/johann/github/LogAnalyzer/Datensätze/Vorbereitete Daten - Beispiel/hdfs_v1/tokenized_list_hdfs.csv",
+    r"/home/johann/github/LogAnalyzer/Datensätze/Vorbereitete Daten - Beispiel/hpc_v1/tokenized_list_hpc.csv",
+    r"/home/johann/github/LogAnalyzer/Datensätze/Vorbereitete Daten - Beispiel/proxifier_v1/tokenized_list_proxifier.csv",
+    r"/home/johann/github/LogAnalyzer/Datensätze/Vorbereitete Daten - Beispiel/zookeeper_v1/tokenized_list_zookeeper.csv"
 ]
 Y_data_files = [
-    r'C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\bgl_v1\label_list_bgl.csv',
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hdfs_v1\label_list_hdfs.csv",
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\hpc_v1\label_list_hpc.csv",
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\proxifier_v1\label_list_proxifier.csv",
-    r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Datensätze\Vorbereitete Daten - Beispiel\zookeeper_v1\label_list_zookeeper.csv"
+    r'Datensätze/Vorbereitete Daten - Beispiel/bgl_v1/label_list_bgl.csv',
+    r"Datensätze/Vorbereitete Daten - Beispiel/hdfs_v1/label_list_hdfs.csv",
+    r"Datensätze/Vorbereitete Daten - Beispiel/hpc_v1/label_list_hpc.csv",
+    r"Datensätze/Vorbereitete Daten - Beispiel/proxifier_v1/label_list_proxifier.csv",
+    r"Datensätze/Vorbereitete Daten - Beispiel/zookeeper_v1/label_list_zookeeper.csv"
 ]
 
 def data_size(file_path):
-    total_lines = sum(1 for line in open(filename))
+    total_lines = sum(1 for line in open(file_path))
     return total_lines
 
 
@@ -64,55 +65,88 @@ def read_from_csv(filename, size, row_count = 0, chunksize = 10000000):
     
     return data, "finish"
 
-row_counter = 0
-size  = data_size(filename)
-print("start")
-while row_counter != "finish":
-    loaded_data, row_counter = read_from_csv(filename, size, row_counter)
+# row_counter = 0
+# size  = data_size(filename)
+# print("start")
+# while row_counter != "finish":
+#     loaded_data, row_counter = read_from_csv(filename, size, row_counter)
     
-# Pad the sequences to ensure uniform input length
-X_data_padded = tf.keras.preprocessing.sequence.pad_sequences(read_from_csv(r"C:\Users\j-u-b\OneDrive\Studium\Semester 6\Bachelorarbeit\Code\LogAnalyzer\Testcode\hdfs\token_list_test.csv"), padding='post')
-np.set_printoptions(threshold=np.inf)
-print(X_data_padded)
-# # Parameter
-# vocab_size = 1001  # Vokabulargröße angepasst für Padding
-# embedding_dim = 64  # Dimension der Embedding-Schicht
-# max_length = len(X_data_padded[0])  # Maximale Länge der Eingabesequenzen
 
-# # Modell
-# model = Sequential([
-#     Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_length),
-#     Bidirectional(GRU(64)),
-#     Dense(1, activation='sigmoid')
-# ])
 
-# # Modellkompilierung
-# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# Modell
+# Laden der Logeinträge aus einer TXT-Datei
+def load_logs(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        logs = file.readlines()
+    return [log.strip() for log in logs]
 
-# # Modellzusammenfassung
-# model.summary()
+# Laden der Labels aus einer CSV-Datei
+def load_labels(file_path):
+    labels = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            labels.append([int(label) for label in row])
+    return labels
 
 
 
+def model_train(logs_file_path, labels_file_path, model, tokenizer, max_length):
+    # Laden der Daten
+    logs = load_logs(logs_file_path)
+    labels = load_labels(labels_file_path)
+    print("Lade "+logs_file_path)
+    # Tokenisierung der Logeinträge
+    sequences = tokenizer.encode_batch(logs)
+    sequences_ids = [encoding.ids for encoding in sequences]
 
-# gen = generator_data('data.csv', batch_size=2, epochs=200)
-# next(gen)
+    # Padding der Sequenzen
+    sequences_padded = pad_sequences(sequences_ids, maxlen=max_length, padding='post')
+    labels_padded = pad_sequences(labels, maxlen=max_length, padding='post')
 
-# # Training
-# X_data_padded = np.array(X_data_padded)
-# Y_data = np.array(Y_data)
-# model.fit(X_data_padded, Y_data, epochs=10, batch_size=2)
+    # Aufteilung in Trainings- und Testdaten
+    X_train, X_test, y_train, y_test = train_test_split(sequences_padded, labels_padded, test_size=0.2, random_state=25)
 
-# # Neue Eingabedaten
-# new_X_data = [[922, 16, 85, 19, 726], [948, 135, 738], [33, 45]]
 
-# # Sequenzen auf dieselbe Länge wie Trainingsdaten padden
-# new_X_data_padded = tf.keras.preprocessing.sequence.pad_sequences(new_X_data, padding='post', maxlen=max_length)
+    
+    # Training des Modells
+    model.fit(X_train, y_train, epochs=10, batch_size=1000, validation_split=0.2)
 
-# # Modell zur Vorhersage verwenden
-# predictions = model.predict(new_X_data_padded)
+tokenizer = Tokenizer.from_file(r"/home/johann/github/LogAnalyzer/Datensätze/Vorbereitete Daten - Beispiel/Tokenizer/tokenizer_BPE.json")
+word_index = tokenizer.get_vocab()
+max_length = 102
+# Erstellung des Modells
+model = Sequential()
+model.add(Embedding(input_dim=len(word_index) + 1, output_dim=64))
+model.add(Bidirectional(GRU(64, return_sequences=True)))
+model.add(TimeDistributed(Dense(64, activation='relu')))
+model.add(Dropout(0.5))
+model.add(TimeDistributed(Dense(1, activation='sigmoid')))
 
-# # Ausgabe der Vorhersagen
-# print("Vorhersagen:")
-# for i, pred in enumerate(predictions):
-#     print(f"Sequenz {i + 1}: {pred[0]:.4f} ({'positiv' if pred[0] >= 0.5 else 'negativ'})")
+# Kompilierung des Modells
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])    
+
+for logs_file_path, labels_file_path in zip(X_data_files, Y_data_files):
+    model_train(logs_file_path, labels_file_path, model, tokenizer, max_length)
+model.save("Model.keras")
+
+# # Evaluation des Modells
+# loss, accuracy = model.evaluate(X_test, y_test)
+# print(f'Loss: {loss}, Accuracy: {accuracy}')
+
+# # Funktion zur Ausgabe der Vorhersagen
+# def predict_and_display(log):
+#     sequence = tokenizer.texts_to_sequences([log])
+#     sequence_padded = pad_sequences(sequence, maxlen=max_length, padding='post')
+#     prediction = model.predict(sequence_padded)[0]
+
+#     words = log.split()
+#     for word, pred in zip(words, prediction):
+#         label = 'nicht statisch' if pred > 0.5 else 'statisch'
+#         print(f'Wort: {word}, Vorhersage: {label}')
+
+# # Beispielvorhersage
+# new_log = "Received block blk_-1111111111111111111 of size 11111 from /11.111.11.111"
+# print("\nVorhersagen für neuen Logeintrag:")
+# predict_and_display(new_log)
+
