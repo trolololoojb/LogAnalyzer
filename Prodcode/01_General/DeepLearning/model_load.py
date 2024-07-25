91f5dfab165ef9d6e7de5c29081931c15fd955bf
trolololoojb <j-u-b+github@outlook.de>
2024-07-25 02:03:40 +0200

    Model load funktional

--- Prodcode/01_General/DeepLearning/model_load.py
+++ Prodcode/01_General/DeepLearning/model_load.py
@@ -1,26 +1,61 @@
 
+import os
 from tokenizers import Tokenizer
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+model_path = r'Datensätze/Vorbereitete Daten - Beispiel/01_Models/20240725-002519**** (gekappter bgl)/tokenizedModel.keras'
+tokenizer_path = r"Datensätze/Vorbereitete Daten - Beispiel/01_Models/20240725-002519**** (gekappter bgl)/tokenizer.json"
+tokenizer = Tokenizer.from_file(tokenizer_path)
+model = load_model(model_path)
+directory_path = os.path.dirname(model_path)
 
-tokenizer = Tokenizer.from_file(r"/home/johann/github/LogAnalyzer/Datensätze/Vorbereitete Daten - Beispiel/Tokenizer/tokenizer.json")
-model = load_model(r'Datensätze/Vorbereitete Daten - Beispiel/Models/20240722-124954_50/tokenizedModel.keras')
+# Eingabelänge extrahieren
+max_length_file_path = os.path.join(directory_path, 'max_length.txt')
 
-max_length=102
+# max_length aus der Datei lesen
+try:
+    with open(max_length_file_path, 'r') as file:
+        max_length = int(file.read().strip())
+    print(f'Die maximale Eingabelänge ist: {max_length}')
+except:
+    max_length = input("Keine max-length Datei! Zum Fortfahren max_length angeben:")
+
 def predict_and_display(log):
-    encode = tokenizer.encode(log)
-    sequence_padded = pad_sequences([encode.ids], maxlen=max_length, padding='post', value = -1)
-    print(encode.tokens)
-    print(sequence_padded)
+
+
+    sequence = [tokenizer.encode(log).ids]
+    sequence_padded = pad_sequences(sequence, maxlen=max_length, padding='post')
+    print(sequence_padded[0])
     prediction = model.predict(sequence_padded)[0]
-    print()
 
-    words = encode.tokens
-    for word, pred in zip(words, prediction):
-        label = 'nicht statisch' if pred > 0.5 else 'statisch'
-        print(f'Wort: {word}, Vorhersage: {label}, Wert: {pred}')
+    words = tokenizer.encode(log).tokens
+    result = []
+    current_label = 'variabel' if prediction[0] > 0 else 'statisch'
+    current_words = [words[0]]
+    current_values = [prediction[0]]
 
-new_log = "9 ddr errors(s) detected and corrected on rank 9, symbol 9, bit 9"
+    for word, pred in zip(words[1:], prediction[1:]):
+        label = 'variabel' if pred > 0 else 'statisch'
+        if label == current_label:
+            current_words.append(word)
+            current_values.append(pred)
+        else:
+            result.append((current_words, current_label, current_values))
+            current_words = [word]
+            current_values = [pred]
+            current_label = label
+
+    # Letzte Gruppe hinzufügen
+    if current_words:
+        result.append((current_words, current_label, current_values))
+
+    # Ausgabe und Schreiben in Datei
+    for group, label, values in result:
+        text = ''.join(group)
+        values_str = ', '.join(map(str, values))
+        print(f'Wörter: {text}, Vorhersage: {label}, Werte: {values_str}')
+
+new_log = input("Log eingeben: ")
 print("\nVorhersagen für neuen Logeintrag:")
 predict_and_display(new_log)
